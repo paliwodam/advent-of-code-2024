fd534777ca97be35a25d3116ec13fd79377428a1
Martyna Paliwoda <martynapaliwoda.2001@gmail.com>
2024-12-05 21:14:57 +0100

    [Day ] Print queue - 2 stars ⭐

--- Chapter-I/Day-5-Print-Queue.py
+++ Chapter-I/Day-5-Print-Queue.py
@@ -1,36 +1,5 @@
 from collections import defaultdict
-input_file = "Chapter-I/Inputs/Day-5-Test-Input.txt"
-
-# with open(input_file) as file:
-#     data = file.read()
-#     rules, updates = data.split("\n\n")
-#     rules, updates = rules.split(), updates.split()
-
-#     before = defaultdict(list)
-#     after = defaultdict(list)
-
-#     for rule in rules:
-#         x, y = rule.split("|")
-#         before[y].append(x)
-#         after[x].append(y)
-
-#     result = 0
-#     for update in updates:
-#         update = update.split(",")
-#         pages = set(update)
-
-#         valid = True
-#         printed = []
-#         for page in update:
-#             for before_page in before[page]:
-#                 if before_page in pages and not before_page in printed:
-#                     valid = False
-#                     break
-#             printed.append(page)
-#         if valid: 
-#             result += int(update[len(update) // 2])
-#     print(result)
-
+input_file = "Inputs/Chapter-I/Day-5-Input.txt"
 
     
 with open(input_file) as file:
@@ -38,33 +7,26 @@ with open(input_file) as file:
     rules, updates = data.split("\n\n")
     rules, updates = rules.split(), updates.split()
 
-    before = defaultdict(list)
+    before = defaultdict(set)
 
     for rule in rules:
         x, y = rule.split("|")
-        before[y].append(x)
+        before[y].add(x)
 
+def print_queue(fix_order=False):
     result = 0
     for update in updates:
         update = update.split(",")
         update_pages = set(update)
+        order = sorted(update, key=lambda x: len(update_pages.intersection(before[x])))
+        if order == update and not fix_order:
+            result += int(order[len(order) // 2])
+        if order != update and fix_order:
+            result += int(order[len(order) // 2])
+    return result
 
-        stack = []
-        for page in update:
-            print(page, ": [", end="")
-            for before_page in before[page]:
-                if before_page in update_pages:
-                    print(before_page, end= ", ")
-            print("]")
+def part1():
+    print(print_queue())
 
-    #     valid = True
-    #     printed = []
-    #     for page in update:
-    #         for before_page in before[page]:
-    #             if before_page in pages and not before_page in printed:
-    #                 valid = False
-    #                 break
-    #         printed.append(page)
-    #     if valid: 
-    #         result += int(update[len(update) // 2])
-    # print(result)
+def part2():
+    print(print_queue(True))
